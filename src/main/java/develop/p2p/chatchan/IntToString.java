e562b6ca6112c1e7708b871b2cefb3bc0cdffeb2
Potato1682 <48394190+Potato1682@users.noreply.github.com>
2020-03-24 06:47:07 +0900

    コード短縮

--- src/main/java/develop/p2p/chatchan/IntToString.java
+++ src/main/java/develop/p2p/chatchan/IntToString.java
@@ -8,6 +8,6 @@ public class IntToString
     public static String getStringFromInt(int i)
     {
         String strlist = "q'a'z'w's'x'e'd'c'r'f'v't'g'b'y'h'n'u'j'm'i'k'o'l'p'1'0'2'9'3'8'4'7'5'6'Q'W'E'R'T'Y'U'I'O'P'L'M'K'N'J'B'H'V'G'C'F'X'D'Z'S'A";
-        return Arrays.asList(strlist.split("'")).get(i).toString();
+        return Arrays.asList("q'a'z'w's'x'e'd'c'r'f'v't'g'b'y'h'n'u'j'm'i'k'o'l'p'1'0'2'9'3'8'4'7'5'6'Q'W'E'R'T'Y'U'I'O'P'L'M'K'N'J'B'H'V'G'C'F'X'D'Z'S'A".split("'")).get(i).toString();
     }
 }
